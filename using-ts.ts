cc4782529db360c539965eeb67a6f3e0387189d2
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-09 21:16:45 +0200

    button selector fix

--- using-ts.ts
+++ using-ts.ts
@@ -1,4 +1,5 @@
-const button = document.querySelector("button");
+const button = document.querySelector("button")!;
+// in addEventListener, we get an error, saying "Object is possibly null". So we need to put an exclamation mark to prove that it is not a nullable object
 const input1 = document.getElementById("num1");
 const input2 = document.getElementById("num2");
 
